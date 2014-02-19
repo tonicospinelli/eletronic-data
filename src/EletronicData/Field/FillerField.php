32199448b5f878cfb14690740d89636c4e3d9074
Antonio Spinelli <tonicospinelli85@gmail.com>
2014-02-19 18:52:04 -0300

    method getFormattedValue from FillerField was refactored

--- src/EletronicData/Field/FillerField.php
+++ src/EletronicData/Field/FillerField.php
@@ -11,10 +11,18 @@ class FillerField extends AbstractField
     public function __construct($startPosition = null, $length = null)
     {
         $this
-            ->setDefaultValue(' ')
+            ->setDefaultValue('0')
             ->setName('filler')
             ->setDescription('Fill with blank')
             ->setStartPosition($startPosition)
             ->setLength($length);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getFormattedValue()
+    {
+        return str_repeat($this->getValue(), $this->getLength());
     }
 }
--- tests/Field/FillerFieldTest.php
+++ tests/Field/FillerFieldTest.php
@@ -27,22 +27,12 @@ class FillerFieldTest extends \PHPUnit_Framework_TestCase
     public function testCreateAFillerField()
     {
         $filler = new FillerField(1, 5);
-        $this->assertEquals(' ', $filler->getDefaultValue());
+        $this->assertEquals('0', $filler->getDefaultValue());
         $this->assertEquals('filler', $filler->getName());
         $this->assertEquals(1, $filler->getStartPosition());
         $this->assertEquals(5, $filler->getLength());
         $this->assertEquals(6, $filler->getEndPosition());
 
-        $format = $this->getMockForAbstractClass('EletronicData\Format\AbstractFormat');
-
-        $format
-            ->expects($this->any())
-            ->method('convert')
-            ->with($this->stringContains(' '))
-            ->will($this->returnValue(' '));
-
-        $filler->setFormat($format);
-
-        $this->assertEquals('     ', $filler->getFormattedValue());
+        $this->assertEquals('00000', $filler->getFormattedValue());
     }
 }
