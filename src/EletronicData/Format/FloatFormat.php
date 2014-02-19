aeafea9759e3b1f5bae7e18251a3d73775376968
Antonio Spinelli <tonicospinelli85@gmail.com>
2014-02-19 18:17:53 -0300

    created a float format component

---
+++ src/EletronicData/Format/FloatFormat.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace EletronicData\Format;
+
+use EletronicData\Format\Exception\ConvertionException;
+use EletronicData\Format\Exception\FormatException;
+
+class FloatFormat extends IntegerFormat
+{
+
+    const SEPARATOR_DOT = '.';
+
+    const SEPARATOR_COMMA = ',';
+
+    /**
+     * @var int
+     */
+    protected $decimalLength;
+
+    /**
+     * @var string
+     */
+    protected $decimalPoint = '.';
+
+    /**
+     * @var string
+     */
+    protected $thousandsSeparator;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->setDecimalLength(2)
+            ->setDecimalPoint(self::SEPARATOR_DOT)
+            ->setThousandsSeparator(self::SEPARATOR_COMMA);
+    }
+
+
+    /**
+     * Sets the decimal length.
+     *
+     * @param int $decimalLength
+     *
+     * @return FloatFormat
+     */
+    public function setDecimalLength($decimalLength)
+    {
+        $this->decimalLength = (int)$decimalLength;
+
+        return $this;
+    }
+
+    /**
+     * Gets the decimal length.
+     * @return int
+     */
+    public function getDecimalLength()
+    {
+        return $this->decimalLength;
+    }
+
+    /**
+     * Sets the decimal point.
+     *
+     * @param string $decimalPoint
+     *
+     * @throws \InvalidArgumentException
+     * @return FloatFormat
+     */
+    public function setDecimalPoint($decimalPoint)
+    {
+        switch ($decimalPoint) {
+            case self::SEPARATOR_COMMA:
+            case self::SEPARATOR_DOT:
+                $this->decimalPoint = $decimalPoint;
+                break;
+            default:
+                throw new \InvalidArgumentException('The given thousand separator ' . $decimalPoint . ' is not recognized');
+        }
+
+
+        return $this;
+    }
+
+    /**
+     * Sets the decimal point.
+     * @return string
+     */
+    public function getDecimalPoint()
+    {
+        return $this->decimalPoint;
+    }
+
+    /**
+     * Sets the thousands separator.
+     *
+     * @param string $thousandsSeparator
+     *
+     * @throws \InvalidArgumentException
+     * @return FloatFormat
+     */
+    public function setThousandsSeparator($thousandsSeparator)
+    {
+        switch ($thousandsSeparator) {
+            case self::SEPARATOR_COMMA:
+            case self::SEPARATOR_DOT:
+                $this->thousandsSeparator = $thousandsSeparator;
+                break;
+            default:
+                throw new \InvalidArgumentException('The given thousand separator ' . $thousandsSeparator . ' is not recognized');
+        }
+
+        return $this;
+    }
+
+    /**
+     * Gets the thousands separator.
+     * @return string
+     */
+    public function getThousandsSeparator()
+    {
+        return $this->thousandsSeparator;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function convert($value)
+    {
+        if (!is_numeric($value)) {
+            throw new ConvertionException('The value given cannot be converted to Float.');
+        }
+
+        return number_format($value, $this->getDecimalLength(), $this->getDecimalPoint(), $this->getThousandsSeparator());
+    }
+}
---
+++ tests/Format/FloatFormatTest.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace EletronicData\Tests\Format;
+
+use EletronicData\Format\FloatFormat;
+
+class FloatFormatTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @var FloatFormat
+     */
+    protected $object;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->object = new FloatFormat();
+    }
+
+    protected function tearDown()
+    {
+        parent::tearDown();
+        $this->object = null;
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testSetAndGetDecimalPoint()
+    {
+        $this->object->setDecimalPoint('.');
+        $this->assertEquals('.', $this->object->getDecimalPoint());
+
+        $this->object->setDecimalPoint(',');
+        $this->assertEquals(',', $this->object->getDecimalPoint());
+
+        $this->object->setDecimalPoint(FloatFormat::SEPARATOR_COMMA);
+        $this->assertEquals(',', $this->object->getDecimalPoint());
+
+        $this->object->setDecimalPoint(FloatFormat::SEPARATOR_DOT);
+        $this->assertEquals('.', $this->object->getDecimalPoint());
+
+        $this->object->setDecimalPoint('-');
+    }
+
+    public function testSetAndGetDecimalLength()
+    {
+        $this->object->setDecimalLength(2);
+        $this->assertEquals(2, $this->object->getDecimalLength());
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testSetAndGetThousandsSeparator()
+    {
+        $this->object->setThousandsSeparator('.');
+        $this->assertEquals('.', $this->object->getThousandsSeparator());
+
+        $this->object->setThousandsSeparator(',');
+        $this->assertEquals(',', $this->object->getThousandsSeparator());
+
+        $this->object->setThousandsSeparator(FloatFormat::SEPARATOR_COMMA);
+        $this->assertEquals(',', $this->object->getThousandsSeparator());
+
+        $this->object->setThousandsSeparator(FloatFormat::SEPARATOR_DOT);
+        $this->assertEquals('.', $this->object->getThousandsSeparator());
+
+        $this->object->setThousandsSeparator('-');
+    }
+
+    public function testGetDefaultData()
+    {
+        $this->assertEquals('.', $this->object->getDecimalPoint());
+        $this->assertEquals(',', $this->object->getThousandsSeparator());
+    }
+
+    public function testApplyWhenIntegerIsGiven()
+    {
+        $this->assertEquals('1.00', $this->object->apply(1));
+    }
+
+    public function testApplyWhenFloatIsGiven()
+    {
+        $this->assertEquals('1.50', $this->object->apply(1.5));
+    }
+
+    public function testApplyWhenDoubleIsGiven()
+    {
+        $this->assertEquals('1.80', $this->object->apply(1.8));
+    }
+
+    public function testApplyWhenDefinedLength()
+    {
+        $this->object->setLength(5);
+        $this->assertTrue('01.00' === $this->object->apply(1));
+    }
+
+    public function testApplyWhenDefinedLengthAndFillRight()
+    {
+        $this->object
+            ->setLength(5)
+            ->setFillOn(FloatFormat::FILL_ON_RIGHT);
+        $this->assertTrue('1.000' === $this->object->apply(1));
+    }
+
+    /**
+     * @expectedException \EletronicData\Format\Exception\ConvertionException
+     */
+    public function testErrorWhenAStringIsGiven()
+    {
+        $this->object->apply('text');
+    }
+
+    /**
+     * @expectedException \EletronicData\Format\Exception\ConvertionException
+     */
+    public function testErrorWhenAnObjectIsGiven()
+    {
+        $this->object->apply(new \stdClass());
+    }
+
+    /**
+     * @expectedException \EletronicData\Format\Exception\ConvertionException
+     */
+    public function testErrorWhenAnArrayIsGiven()
+    {
+        $this->object->apply(array());
+    }
+}
