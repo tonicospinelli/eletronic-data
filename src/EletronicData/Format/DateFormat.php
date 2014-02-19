78f8344c6b54595097cb8873d74c02abf6667ee4
Antonio Spinelli <tonicospinelli85@gmail.com>
2014-02-19 15:59:25 -0300

    created a date format component

---
+++ src/EletronicData/Format/DateFormat.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace EletronicData\Format;
+
+use EletronicData\Format\Exception\ConvertionException;
+
+class DateFormat extends AbstractFormat
+{
+
+    const FORMAT_YYYYMMDD = 'Ymd';
+
+    const FORMAT_DDMMYYYY = 'dmY';
+
+    const FORMAT_MMDDYYYY = 'mdY';
+
+    protected $format;
+
+    function __construct()
+    {
+        $this->setFormat(self::FORMAT_YYYYMMDD);
+    }
+
+    /**
+     * Sets format to convert the date.
+     *
+     * @param string $format can be DateFormat::FORMAT_YYYYMMDD, DateFormat::FORMAT_DDMMYYYY,
+     *                       or DateFormat::FORMAT_YYYYMMDD. If it is not specified it is assumed
+     *                       to be DateFormat::FORMAT_YYYYMMDD.
+     *
+     * @return DateFormat
+     * @throws \InvalidArgumentException
+     */
+    public function setFormat($format)
+    {
+        switch ($format) {
+            case self::FORMAT_DDMMYYYY:
+            case self::FORMAT_MMDDYYYY:
+            case self::FORMAT_YYYYMMDD:
+                $this->format = $format;
+                break;
+            default:
+                throw new \InvalidArgumentException('The given format ' . $format . ' is not recognized');
+        }
+        return $this;
+    }
+
+    /**
+     * Gets format to convert the date.
+     * @return string
+     */
+    public function getFormat()
+    {
+        return $this->format;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function convert($value)
+    {
+        if (!($value instanceof \DateTime)) {
+            $value = preg_replace('/[T\s].*$/', '', $value);
+            $value = \DateTime::createFromFormat('Y-m-d', $value);
+        }
+
+        if (!($value instanceof \DateTime)) {
+            throw new ConvertionException('The value given cannot be converted to \DateTime object.');
+        }
+
+        return $value->format($this->getFormat());
+    }
+}
---
+++ tests/Format/DateFormatTest.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace EletronicData\Tests\Format;
+
+use EletronicData\Format\DateFormat;
+
+class DateFormatTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @var DateFormat
+     */
+    protected $object;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->object = new DateFormat();
+    }
+
+    protected function tearDown()
+    {
+        parent::tearDown();
+        $this->object = null;
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testSetAndGetFormat()
+    {
+        $this->object->setFormat(DateFormat::FORMAT_DDMMYYYY);
+        $this->assertEquals('dmY', $this->object->getFormat());
+
+        $this->object->setFormat(DateFormat::FORMAT_MMDDYYYY);
+        $this->assertEquals('mdY', $this->object->getFormat());
+
+        $this->object->setFormat(DateFormat::FORMAT_YYYYMMDD);
+        $this->assertEquals('Ymd', $this->object->getFormat());
+
+        $this->object->setFormat('Y-m-d');
+    }
+
+    public function testApplyDefaultFormat()
+    {
+        $date = new \DateTime('2014-02-01');
+        $this->assertEquals('20140201', $this->object->apply($date));
+    }
+
+    public function testApplyYearMonthDayFormat()
+    {
+        $date = new \DateTime('2014-02-01');
+        $this->object->setFormat(DateFormat::FORMAT_YYYYMMDD);
+        $this->assertEquals('20140201', $this->object->apply($date));
+    }
+
+    public function testApplyDayMonthYearFormat()
+    {
+        $date = new \DateTime('2014-02-01');
+        $this->object->setFormat(DateFormat::FORMAT_DDMMYYYY);
+        $this->assertEquals('01022014', $this->object->apply($date));
+    }
+
+    public function testApplyMonthDayYearFormat()
+    {
+        $date = new \DateTime('2014-02-01');
+        $this->object->setFormat(DateFormat::FORMAT_MMDDYYYY);
+        $this->assertEquals('02012014', $this->object->apply($date));
+    }
+
+    public function testApplyFormatFromString()
+    {
+        $date = '2014-02-01';
+        $this->assertEquals('20140201', $this->object->apply($date));
+    }
+
+    /**
+     * @expectedException \EletronicData\Format\Exception\ConvertionException
+     */
+    public function testErrorOnApplyApplyFormat()
+    {
+        $date = '01-02-2014';
+        $this->object->apply($date);
+    }
+
+    public function testApplyFormatFromFullDateString()
+    {
+        $date = '2014-02-01 00:00:00';
+        $this->assertEquals('20140201', $this->object->apply($date));
+    }
+
+    public function testApplyFormatFromIso8601()
+    {
+        $date = '2014-02-01T00:00:00';
+        $this->assertEquals('20140201', $this->object->apply($date));
+    }
+}
